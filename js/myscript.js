60ade799411b9c7a561b4423d37c5cd2ee34037e
Antoha5001 <plantsvzzombi@mail.ru>
2016-11-13 21:40:29 +0700

    edit code

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    
+    setTimeout(function(){
+        $('.header').addClass('is-visible');
+    },500); 
+    setTimeout(function(){
+        $('.header-info').addClass('is-visible');
+    },1200);   
+    
+    
+    
+});
